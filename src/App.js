83a8d28804610df598e63ad7081066c52f62d935
unknown <ingdavidavila@hotmail.com>
2023-04-11 02:17:49 -0500

    inicial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import NavScrollExample from "../src/Components/Navbar";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function App(){
+    return(
+        <div className='App'>
+            <NavScrollExample />
+            <header className='header'>
+            <div id="one">
+                        <p>
+                            <a>We Are Buying Your Bulk</a><br />
+                            <a>Common/Uncommon</a><br />
+                            <a>7usd per 1K cards</a><br />
+                            <a>Rares/Mythics</a><br />
+                            <a>.07 cents per card</a><br />
+
+                        </p>
+                    </div>
+                    <div id="two">
+                            <a>We Are Buying Your Bulk</a><br />
+                            <a>Common/Uncommon</a><br />
+                            <a>7usd per 1K cards</a><br />
+                            <a>Reverse Holo</a><br />
+                            <a>.07 cents per card</a><br />
+                    </div>
+                    <div id="Three"></div>
+            </header>
+        </div>
+
+    )
 }
 
 export default App;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import './Navbar.css';
+
+function NavbarscrollExample() {
+    return (
+      <Navbar expand='lg'>
+        <Container fluid >
+          <Navbar.Brand href="/"><img src='./GJlogo.png' alt="logo" className='logo-nav'/></Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              navbarScroll
+            >
+              
+              <NavDropdown title="Magic The Gathering" id="navbarScrollingDropdown">
+                <NavDropdown.Item href="#action3">Sealed Product</NavDropdown.Item>
+                <NavDropdown.Item href="#one">
+                  Singles
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action5">
+                  Sell Us Your Cards
+                </NavDropdown.Item>
+              </NavDropdown>
+              <NavDropdown title="Pokemon" id="navbarScrollingDropdown">
+                <NavDropdown.Item href="#action3">Sealed Product</NavDropdown.Item>
+                <NavDropdown.Item href="#two">
+                  Singles
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action5">
+                  Sell Us Your Cards
+                </NavDropdown.Item>
+              </NavDropdown>
+              <NavDropdown title="Yu-Gi-Oh" id="navbarScrollingDropdown">
+                <NavDropdown.Item href="#action3">Sealed Product</NavDropdown.Item>
+                <NavDropdown.Item href="#action4">
+                  Singles
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action5">
+                  Sell Us Your Cards
+                </NavDropdown.Item>
+              </NavDropdown>
+              <Nav.Link href="/about" >
+                About us
+              </Nav.Link>
+              <Nav.Link href="/contact" >
+                Contact
+              </Nav.Link>
+            </Nav>
+            <Form className="d-flex">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    );
+  }
+  
+  export default NavbarscrollExample;
